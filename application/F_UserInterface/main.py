d43edbc0e4ccef55494574824264ede1132d437d
Noudke <noudyo@outlook.com>
2023-05-25 22:34:00 +0200

    Changed UI.
    The user can now drag and drop preprocessing steps in order to execute them during analysis.

--- application/F_UserInterface/main.py
+++ application/F_UserInterface/main.py
@@ -12,41 +12,44 @@ app.secret_key = 'your_secret_key_here'  # Change to your actual secret key
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        source_type = request.form.get('source_type')
-        link = request.form.get('link')
-        preprocessing_steps = request.form.getlist('preprocessing_steps')
-        functionality = request.form.get('functionality')
+        session['source_type'] = request.form.get('source_type')
+        session['link'] = request.form.get('link')
+        session['preprocessing_steps'] = request.form.get('preprocessing_steps_order').split(",")
+        session['functionality'] = request.form.get('functionality')
 
-        if source_type == 'Online forum discussion':
-            message_class = request.form.get('message_class')
-            pagination_class = request.form.get('pagination_class')
-            message_text_class = request.form.get('message_text_class')
-            message_author_class = request.form.get('message_author_class')
-            analyzer = ForumAnalyzer(link, message_class, False, pagination_class, message_text_class,
-                                     message_author_class)
-        else:
-            analyzer = ScientificLiteratureAnalyzer(link)
+        if session['source_type'] == 'Online forum discussion':
+            session['message_class'] = request.form.get('message_class')
+            session['pagination_class'] = request.form.get('pagination_class')
+            session['message_text_class'] = request.form.get('message_text_class')
+            session['message_author_class'] = request.form.get('message_author_class')
 
-        result = analyzer.analyze(functionality=functionality, preprocessing_steps=preprocessing_steps)
-        session['result'] = result
-
-        def background_analysis():
-            result = analyzer.analyze(functionality=functionality, preprocessing_steps=preprocessing_steps)
-            session['result'] = result
-
-        # Start the background task and then immediately redirect to the loading page
-        threading.Thread(target=background_analysis).start()
         return redirect(url_for('loading'))
 
     return render_template('home.html')
 
-
 @app.route('/loading', methods=['GET'])
 def loading():
-    return render_template('loading.html')
+    functionality = session['functionality']
+    preprocessing_steps = session['preprocessing_steps']
 
+    if session['source_type'] == 'Online forum discussion':
+        analyzer = ForumAnalyzer(session['link'], session['message_class'], False, session['pagination_class'],
+                                 session['message_text_class'],
+                                 session['message_author_class'])
+    else:
+        analyzer = ScientificLiteratureAnalyzer(session['link'])
+
+    print(preprocessing_steps)
+
+    result = analyzer.analyze(functionality=functionality, preprocessing_steps=preprocessing_steps)
+    session['result'] = result
+
+    return redirect(url_for('result'))
 
 @app.route('/result')
 def result():
     result = session.get('result')
     return render_template('result.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
