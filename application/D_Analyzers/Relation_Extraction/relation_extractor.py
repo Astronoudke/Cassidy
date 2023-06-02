c324cf42d4cafa74b7e86de05938fd5ed6bf2eaa
Noudke <skynoudyo.fm@gmail.com>
2023-06-02 11:30:13 +0200

    Edited relation extraction
    TF-IDF relation extraction now determines relations not necessarily of bigrams, but of words that appear together in a sentence, but not necessarily next to one another.

--- application/D_Analyzers/Relation_Extraction/relation_extractor.py
+++ application/D_Analyzers/Relation_Extraction/relation_extractor.py
@@ -36,23 +36,25 @@ class RelationExtractor:
         # Flatten the list of sentences to feed to the vectorizer
         all_nouns = [noun for sublist in sentences for noun in sublist]
 
-        # Vectorize the text using TF-IDF with bigrams
-        tfidf_vectorizer = TfidfVectorizer(ngram_range=(2, 2))
+        # Vectorize the text using TF-IDF
+        tfidf_vectorizer = TfidfVectorizer()
         tfidf_matrix = tfidf_vectorizer.fit_transform(all_nouns)
 
-        # Create a dictionary to hold the scores for each bigram
-        tfidf_scores = defaultdict(int)
+        # Create a dictionary to hold the scores for each noun
+        tfidf_scores = {noun: tfidf_matrix[0, index] for noun, index in tfidf_vectorizer.vocabulary_.items()}
 
-        # Go through each bigram and its score
-        for bigram, index in tfidf_vectorizer.vocabulary_.items():
-            score = tfidf_matrix.sum(axis=0).tolist()[0][index]
-            tfidf_scores[bigram] = score
+        # Go through each sentence and generate all pairings of nouns
+        noun_pairs_scores = defaultdict(int)
+        for sublist in sentences:
+            for pair in combinations(sublist, 2):
+                # Compute the score for each pair as the sum or product of individual TF-IDF scores
+                pair_score = tfidf_scores.get(pair[0], 0) * tfidf_scores.get(pair[1], 0)  # or use + for sum
+                noun_pairs_scores[pair] += pair_score
 
         # Sort the scores in descending order
-        sorted_scores = sorted(tfidf_scores.items(), key=lambda x: x[1], reverse=True)
+        sorted_scores = sorted(noun_pairs_scores.items(), key=lambda x: x[1], reverse=True)
 
-        # Return the top n bigrams without their scores
-        top_bigrams = [tuple(bigram.split(' ')) for bigram, score in sorted_scores[:top_n]]
+        # Return the top n bigrams
+        top_bigrams = [bigram for bigram, score in sorted_scores[:top_n]]
         print(top_bigrams)
         return top_bigrams
-
